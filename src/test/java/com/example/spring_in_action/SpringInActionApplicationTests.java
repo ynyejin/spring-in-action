0e040736bc61b35c9489f0c234b252696308d001
ynyejin <yyj030625@gmail.com>
2026-01-13 19:57:59 +0900

    initialize Spring Boot project

---
+++ src/test/java/com/example/spring_in_action/SpringInActionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_in_action;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringInActionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
